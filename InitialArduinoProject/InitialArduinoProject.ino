e85e66c388abb0f05982052174118da0a344c339
Theo Stangebye - Merrell <theostangebye@gmail.com>
2016-10-13 22:05:00 -0400

    Init Arduino Project
    This branch is Theo’s creation of the Arduino project file.

---
+++ InitialArduinoProject/InitialArduinoProject.ino
@@ -0,0 +1,11 @@
+//This is a default Arduino File for theo to get more practicing merging branches.
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
